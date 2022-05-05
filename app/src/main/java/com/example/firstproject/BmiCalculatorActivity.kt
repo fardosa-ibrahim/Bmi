6b7ceda138511c736ce4c319b7303a783e393199
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-05-05 20:20:21 +0300

    first commit

---
+++ app/src/main/java/com/example/firstproject/BmiCalculatorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BmiCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi_calculator)
+    }
+}
---
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.firstproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnBmi:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnBmi=findViewById(R.id.btnBmi)
+        btnBmi.setOnClickListener {
+            val intent=Intent(this,BmiCalculatorActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
